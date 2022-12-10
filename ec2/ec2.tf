231fbc22572be7a8b3a7190a8ea4fb533109a9ba
Vamsi Chunduru <cvamsikrishna11@gmail.com>
2022-12-10 14:20:48 -0500

    code improvments

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -63,18 +63,18 @@ resource "aws_iam_instance_profile" "instance_profile" {
 resource "aws_instance" "web_server" {
   ami                    = "ami-0b0dcb5067f052a63"
   instance_type          = "t3.small"
-  key_name               = var.key_pair_name  
+  key_name               = var.key_pair_name
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
   user_data              = file("scripts/userdata.sh")
   iam_instance_profile   = aws_iam_instance_profile.instance_profile.name
-  tags                   = merge(var.tags, { Name = join("", [var.name, "-", "webserver"]) }, {Environment = var.name})
+  tags                   = merge(var.tags, { Name = join("", [var.name, "-", "webserver"]) }, { Environment = var.name })
 
   # best practices as per checkov scanner
 
-  # monitoring = true
-  # ebs_optimized = true
-  # root_block_device {
-  # encrypted     = true
-  # }
+  monitoring    = true
+  ebs_optimized = true
+  root_block_device {
+    encrypted = true
+  }
 
 }
