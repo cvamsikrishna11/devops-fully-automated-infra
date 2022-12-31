67d8b2602e6b9c6fe83aad76ee315982f031ba93
sydon14 <sydon_md@yahoo.com>
2022-12-31 17:29:25 -0500

    adding syd-dev-test-infra project

---
+++ syd-local.tf
@@ -0,0 +1,9 @@
+locals {
+  common_tags={
+    company = "jjtech"
+    owner = "jjtech DevOps Team"
+    team-email = "sydon_md@yahoo.com"
+    time = formatdate("DD MMM YYYY hh:mm ZZZ", timestamp())    
+  }
+ 
+}
